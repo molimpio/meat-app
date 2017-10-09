e2b0656666bffbc32e897bfeda99f3391b4439e1
Moises <moises@rcbdesenvolvimento.com.br>
2017-10-09 09:42:12 -0300

    adicionado detalhes do restaurante

--- out-tsc/app/app/app.routes.js
+++ out-tsc/app/app/app.routes.js
@@ -1,9 +1,11 @@
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { RestaurantsComponent } from './restaurants/restaurants.component';
+import { RestaurantDetailComponent } from "./restaurant-detail/restaurant-detail.component";
 export var ROUTES = [
     { path: '', component: HomeComponent },
     { path: 'about', component: AboutComponent },
-    { path: 'restaurants', component: RestaurantsComponent }
+    { path: 'restaurants', component: RestaurantsComponent },
+    { path: 'restaurants/:id', component: RestaurantDetailComponent }
 ];
 //# sourceMappingURL=app.routes.js.map
