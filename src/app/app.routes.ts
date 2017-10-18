de51070a19545fae9e20670099c7f24e1980aee5
Moises <molimpio@hotmail.com.br>
2017-10-18 07:18:38 -0200

    component finalizar compra

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,15 +6,17 @@ import {RestaurantsComponent} from './restaurants/restaurants.component'
 import {RestaurantDetailComponent} from "./restaurant-detail/restaurant-detail.component"
 import {MenuComponent} from './restaurant-detail/menu/menu.component'
 import {ReviewsComponent} from './restaurant-detail/reviews/reviews.component'
+import {OrderComponent} from './order/order.component'
 
 export const ROUTES: Routes = [
-    {path: '', component: HomeComponent},
-    {path: 'about', component: AboutComponent},
-    {path: 'restaurants', component: RestaurantsComponent},
-    {path: 'restaurants/:id', component: RestaurantDetailComponent,
-        children: [
-            {path: '', redirectTo: 'menu', pathMatch:'full'},
-            {path: 'menu', component: MenuComponent},
-            {path: 'reviews', component: ReviewsComponent}
-    ]}
+  {path: '', component: HomeComponent},
+  {path: 'about', component: AboutComponent},
+  {path: 'restaurants', component: RestaurantsComponent},
+  {path: 'restaurants/:id', component: RestaurantDetailComponent,
+  children: [
+    {path: '', redirectTo: 'menu', pathMatch:'full'},
+    {path: 'menu', component: MenuComponent},
+    {path: 'reviews', component: ReviewsComponent}
+  ]},
+  {path: 'order', component: OrderComponent},
 ]
