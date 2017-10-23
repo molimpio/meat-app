45397e8f902f86d20a54378f9bb2fa47da152a9a
Moises <moises@rcbdesenvolvimento.com.br>
2017-10-23 12:55:14 -0200

    criou animações para snackbar

--- out-tsc/app/app/shared/messages/snackbar/snackbar.component.js
+++ out-tsc/app/app/shared/messages/snackbar/snackbar.component.js
@@ -8,9 +8,11 @@ var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
 import { Component } from '@angular/core';
+import { trigger, state, style, transition, animate } from "@angular/animations";
 var SnackbarComponent = (function () {
     function SnackbarComponent() {
         this.message = 'Helo There!';
+        this.snackVisibility = 'hidden';
     }
     SnackbarComponent.prototype.ngOnInit = function () {
     };
@@ -20,7 +22,21 @@ SnackbarComponent = __decorate([
     Component({
         selector: 'mt-snackbar',
         templateUrl: './snackbar.component.html',
-        styleUrls: ['./snackbar.component.css']
+        styleUrls: ['./snackbar.component.css'],
+        animations: [
+            trigger('snack-visibility', [
+                state('hidden', style({
+                    opacity: 0,
+                    bottom: '0px'
+                })),
+                state('visible', style({
+                    opacity: 1,
+                    bottom: '30px'
+                })),
+                transition('hidden => visible', animate('500ms 0s ease-in')),
+                transition('visible => hidden', animate('500ms 0s ease-out')),
+            ])
+        ]
     }),
     __metadata("design:paramtypes", [])
 ], SnackbarComponent);
