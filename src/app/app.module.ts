5a9536f9c73deb6eda799047ab6a373fe274adf2
Moises <molimpio@hotmail.com.br>
2017-10-23 07:37:06 -0200

    criando modulo de compras

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -21,10 +21,7 @@ import { MenuItemComponent } from './restaurant-detail/menu-item/menu-item.compo
 import { ReviewsComponent } from './restaurant-detail/reviews/reviews.component'
 
 import {ShoppingCartService} from './restaurant-detail/shopping-cart/shopping-cart.service';
-import { OrderComponent } from './order/order.component';
-import { OrderItemsComponent } from './order/order-items/order-items.component'
 import {OrderService} from "./order/order.service";
-import { DeliveryCostsComponent } from './order/delivery-consts/delivery-costs.component';
 import { OrderSummaryComponent } from './order-summary/order-summary.component';
 
 import {SharedModule} from './shared/shared.module'
@@ -41,10 +38,7 @@ import {SharedModule} from './shared/shared.module'
     MenuComponent,
     ShoppingCartComponent,
     MenuItemComponent,
-    ReviewsComponent,
-    OrderComponent,
-    OrderItemsComponent,
-    DeliveryCostsComponent,
+    ReviewsComponent,    
     OrderSummaryComponent
   ],
   imports: [
