4214fbeada9d95cf57a1886627ae7312e3d421fb
Moises <moises@rcbdesenvolvimento.com.br>
2017-10-23 09:51:09 -0200

    configuracao modulo animations

--- out-tsc/app/app/app.module.js
+++ out-tsc/app/app/app.module.js
@@ -8,6 +8,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, LOCALE_ID } from '@angular/core';
 import { HttpModule } from '@angular/http';
 import { RouterModule, PreloadAllModules } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ROUTES } from './app.routes';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -43,6 +44,7 @@ AppModule = __decorate([
         ],
         imports: [
             BrowserModule,
+            BrowserAnimationsModule,
             HttpModule,
             SharedModule.forRoot(),
             RouterModule.forRoot(ROUTES, { preloadingStrategy: PreloadAllModules })
