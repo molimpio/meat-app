a5cbe8a15479d6efba671770d897e53632516c44
Moises <moises@rcbdesenvolvimento.com.br>
2017-10-25 14:03:20 -0200

    logango user backend

--- backend/dist/server.js
+++ backend/dist/server.js
@@ -1,7 +1,9 @@
 "use strict";
+exports.__esModule = true;
 var jsonServer = require("json-server");
 var fs = require("fs");
 var https = require("https");
+var auth_1 = require("./auth");
 var server = jsonServer.create();
 var router = jsonServer.router('db.json');
 var middlewares = jsonServer.defaults();
@@ -10,6 +12,8 @@ server.use(middlewares);
 // To handle POST, PUT and PATCH you need to use a body-parser
 // You can use the one used by JSON Server
 server.use(jsonServer.bodyParser);
+// middleware para login
+server.post('/login', auth_1.handleAuthentication);
 // Use default router
 server.use(router);
 var options = {
