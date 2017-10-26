f20003584b2c5316d7f7d46cd290dcea20442ae2
Moises <moises@rcbdesenvolvimento.com.br>
2017-10-26 09:21:26 -0200

    validacao de token jwt

--- backend/authz.ts
+++ backend/authz.ts
@@ -1,17 +1,19 @@
 import {Request, Response} from "express"
 import * as jwt from "jsonwebtoken"
+import {apiConfig} from "./api-config";
 
 export const handleAuthorization = (req: Request, resp: Response, next) => {
   const token = extractToken(req)
+  console.log(token)
   if(!token) {
     resp.setHeader('WWW-Authenticate', 'Bearer token_type="JWT"')
     resp.status(401).json({message: 'Você precisa se autenticar.'})
   } else {
-    jwt.verify(token, 'meat-api-password', (error, decoded) => {
+    jwt.verify(token, apiConfig.secret, (error, decoded) => {
       if(decoded) {
         next()
       } else {
-        res.status(403).json({message: 'Não autorizado.'})
+        resp.status(403).json({message: 'Não autorizado.'})
       }
     })
   }
@@ -21,7 +23,7 @@ function extractToken(req: Request): string {
   let token = undefined
   if(req.headers && req.headers.authorization) {
     // Authorization:Bearer zzz.zzz.zzz
-    const parts: string[] = req.headers.authorization.split('')
+    const parts: string[] = req.headers.authorization.split(' ')
     if(parts.length === 2 && parts[0] === 'Bearer') {
       token = parts[1]
     }
